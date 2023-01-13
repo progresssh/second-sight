6f3cff20e965c49856680092b995f99ac9744503
progress <star@progress.sh>
2023-01-13 09:54:28 +0100

    added more SPA routing catch-all

--- src/renderer/_default.page.server.tsx
+++ src/renderer/_default.page.server.tsx
@@ -1,7 +1,10 @@
-<!DOCTYPE html>
-<html lang="en" class="h-full">
+import { escapeInject } from "vite-plugin-ssr"
 
-<head>
+export { render }
+
+async function render() {
+  return escapeInject`<html lang="en" class="h-full">
+  <head>
   <meta charset="UTF-8" />
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -10,12 +13,9 @@
   <link rel="icon" type="image/svg+xml" href="/vite.svg" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title>Second Sight</title>
-
-</head>
-
-<body class="h-full font-sans bg-[#232946]">
-  <div id="root" class="h-full"></div>
-  <script type="module" src="/src/main.tsx"></script>
-</body>
-
-</html>
+  </head>
+  <body class="h-full font-sans bg-[#232946]">
+    <div id="react-root" class="h-full"></div>
+  </body>
+</html>`
+}
