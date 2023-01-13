746ff5e5af6304f908ed29adf459942d86576218
progress <star@progress.sh>
2023-01-13 11:36:52 +0100

    added routing if logged in

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -1,5 +1,12 @@
+import { useNavigate } from "react-router-dom"
 import App from "../App"
+import { useAuthContext } from "../utils/context/AuthContext"
 
 export default function Root() {
+  const { user } = useAuthContext()
+  const navigate = useNavigate()
+  if (user) {
+    navigate("/diary")
+  }
   return <App />
 }
