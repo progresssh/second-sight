1aabe58a3561f84af696cb038b36442e196ec67e
progress <star@progress.sh>
2023-01-18 16:09:25 +0100

    removed isLoading

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -1,18 +1,5 @@
-import { useState } from "react"
-import { useNavigate } from "react-router-dom"
 import App from "../App"
-import { useAuthContext } from "../utils/context/AuthContext"
 
 export default function Root() {
-  const { user } = useAuthContext()
-  const navigate = useNavigate()
-  const [isLoading, setIsLoading] = useState(true)
-
-  if (user) {
-    navigate("/diary")
-  }
-
-  setIsLoading(false)
-
-  return !isLoading ? <App /> : <div>hey</div>
+  return <App />
 }
